7dd79be5640db0d724adcfb5c9184f5313e43e42
John Torres <jandrewtorres5@gmail.com>
2018-02-26 14:34:07 -0800

    first commit

---
+++ src/main/java/term3_part2/WeightConversion.java
@@ -0,0 +1,14 @@
+package term3_part2;
+
+public class WeightConversion {
+
+	public double KilosToPounds (double kilos){
+		
+		return (kilos / 0.454);
+	}
+	
+	public double PoundsToKilos (double pounds){
+		
+		return (pounds * 0.454);
+	}
+}
